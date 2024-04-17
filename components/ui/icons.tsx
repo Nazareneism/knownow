4dc223c7d8f35d970135ffb86da231f2c5a05d64
karmlin <kennetharmlin@icloud.com>
2024-04-17 09:16:59 -0400

    Update icons.tsx
    reverted

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -1,17 +1,22 @@
 'use client'
 
-import React from 'react'; // Ensure React is in scope when using JSX
 import { cn } from '@/lib/utils'
 
-function IconLogo({ className, ...props }: React.ImgHTMLAttributes<HTMLImageElement>) {
+function IconLogo({ className, ...props }: React.ComponentProps<'svg'>) {
   return (
-    <img
-      src="app/favicon.ico" // Adjust the path as necessary
-      alt="Logo"
+    <svg
+      fill="currentColor"
+      viewBox="0 0 256 256"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
       className={cn('h-4 w-4', className)}
       {...props}
-    />
-  );
+    >
+      <circle cx="128" cy="128" r="128" fill="black"></circle>
+      <circle cx="102" cy="128" r="18" fill="white"></circle>
+      <circle cx="154" cy="128" r="18" fill="white"></circle>
+    </svg>
+  )
 }
 
-export { IconLogo };
+export { IconLogo }
