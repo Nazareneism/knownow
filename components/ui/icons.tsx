97bbd2d3584f845166753f550d6c73a6c03b2d09
Naz <145207655+Nazareneism@users.noreply.github.com>
2024-07-28 01:38:48 -0400

    Moved to public and changed path of logo

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -7,7 +7,7 @@ function IconLogo({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div className={cn('h-4 w-4 relative', className)} {...props}>
       <Image
-        src="../app/Logo1.png"
+        src="/Logo1.png"
         alt="Logo"
         width={16}
         height={16}
