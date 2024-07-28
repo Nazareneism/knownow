74a1ddafd60241682fc52f08424bd9274fc5614a
Naz <145207655+Nazareneism@users.noreply.github.com>
2024-07-28 01:24:26 -0400

    Added .. to icon path

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -7,7 +7,7 @@ function IconLogo({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div className={cn('h-4 w-4 relative', className)} {...props}>
       <Image
-        src="/app/favicon.ico"
+        src="../app/favicon.ico"
         alt="Logo"
         width={16}
         height={16}
